00dd6538059f5706e24bc211f5d136ee594a28f0
Sunjingxin <1825951386@qq.com>
2016-07-05 18:47:21 +0800

    0

--- amd_demo/mod/home.js
+++ amd_demo/mod/home.js
@@ -13,7 +13,7 @@ define(["text!../../tpl/home.html","./weixin.js"],function(html){
                 var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
                 var speed = res.speed; // 速度，以米/每秒计
                 var accuracy = res.accuracy; // 位置精度
-                alert(latitude+"|"+longitude);
+                alert("您的纬度"+latitude+"|"+"您的经度"+longitude);
                 callback();
             },
             fail:function(){
